7c5c76fed7f63e26f96ab789b0ea77d4ac8efb05
Eldrich Raygoza <raygozaeldrich@gmail.com>
2025-01-16 23:55:02 -0600

    Crear un paquete

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const characters = [
+    "Link", 
+    "Zelda", 
+    "Ganon", 
+    "Navi", 
+    "Impa", 
+    "Midna", 
+    "Darunia", 
+    "Rauru", 
+    "Saria", 
+    "Nabooru", 
+    "Skull Kid", 
+    "Tingle", 
+    "Dark Link", 
+    "Vaati", 
+    "Hilda", 
+    "Tetra", 
+    "Ganondorf", 
+    "Fi", 
+    "Ezlo", 
+    "Linebeck", 
+    "Zant", 
+    "Ghirahim", 
+    "Pipit", 
+    "Groose", 
+    "Faron", 
+    "Yunobo", 
+    "Sidon", 
+    "Revali", 
+    "Daruk", 
+    "Mipha", 
+    "The Great Deku Tree", 
+    "The Wind Fish", 
+    "Makar", 
+    "Koume", 
+    "Kotake", 
+    "Bladon", 
+    "The King of Red Lions", 
+    "Agahnim", 
+    "Ravio", 
+    "Master Kohga", 
+    "Hilda", 
+    "Master Sword", 
+    "Epona", 
+    "Ciela", 
+    "Princess Ruto", 
+    "Talon", 
+    "Ingo", 
+    "King Daphnes"
+];
+
+const zeldaCharacter = () => {
+    const character = characters[Math.floor(Math.random * messages.length)];
+    console.log(`\x1b[34m${character}\x1b[89m`);
+}
+
+module.exports = {
+    zeldaCharacter
+};
